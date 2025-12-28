2b4b2b55965d7da4bf5fcc69c03d1ea922216564
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2025-12-28 08:39:18 +0200

    Added Home Menu Screen

---
+++ src/main/java/com/mycompany/clientside/controllers/HomeMenuController.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.clientside.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+/**
+ * FXML Controller class
+ *
+ * @author lenovo
+ */
+public class HomeMenuController implements Initializable {
+
+
+    @FXML
+    private Button logOutButton;
+    @FXML
+    private Button singleModeButton;
+    @FXML
+    private Button localModeButton;
+    @FXML
+    private Button onlineModeButton;
+    @FXML
+    private Button scoreButton;
+    @FXML
+    private Button replayButton;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void onLogOut(ActionEvent event) {
+    }
+
+    @FXML
+    private void onSingleClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void onLocalModeClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void onOnlineModeClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void onScoreClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void onReplayClick(ActionEvent event) {
+    }
+
+}
