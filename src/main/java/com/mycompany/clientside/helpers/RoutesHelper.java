d70547bad1a1e69b1c6210805dafc7d1468ce8b2
Eldeep1 <alieldeep111@gmail.com>
2025-12-29 07:11:52 +0200

    adding routes helper

---
+++ src/main/java/com/mycompany/clientside/helpers/RoutesHelper.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clientside.helpers;
+
+/**
+ *
+ * @author Depogramming
+ */
+public class RoutesHelper {
+
+    public static final String HOME_MENU = "/com/mycompany/clientside/fxml/homeMenu.fxml";
+    public static final String PLAYER_STATE = "/com/mycompany/clientside/stats/PlayerState.fxml";
+}
