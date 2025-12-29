8994dd758bf26db6d7c2545d12a815ea3e4dc13a
Eldeep1 <alieldeep111@gmail.com>
2025-12-29 07:13:49 +0200

    implementing onScoreClick function

--- src/main/java/com/mycompany/clientside/controllers/HomeMenuController.java
+++ src/main/java/com/mycompany/clientside/controllers/HomeMenuController.java
@@ -4,19 +4,26 @@
  */
 package com.mycompany.clientside.controllers;
 
+import com.mycompany.clientside.helpers.NavigationHelper;
+import com.mycompany.clientside.helpers.RoutesHelper;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 /**
  * FXML Controller class
  *
  * @author lenovo
  */
 public class HomeMenuController implements Initializable {
-
 
     @FXML
     private Button logOutButton;
@@ -30,14 +37,15 @@ public class HomeMenuController implements Initializable {
     private Button scoreButton;
     @FXML
     private Button replayButton;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
     @FXML
     private void onLogOut(ActionEvent event) {
     }
@@ -56,6 +64,15 @@ public class HomeMenuController implements Initializable {
 
     @FXML
     private void onScoreClick(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(RoutesHelper.PLAYER_STATE));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            NavigationHelper.navigateTo(stage, root);
+        } catch (IOException ex) {
+            //show dialog or something
+            ex.printStackTrace();
+        }
     }
 
     @FXML
