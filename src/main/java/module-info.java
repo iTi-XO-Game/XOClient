4ecc5b2ab129b5b2d973f456a3eef11d451d2576
Eldeep1 <alieldeep111@gmail.com>
2026-01-02 06:35:09 +0200

    setting up dependencies and configurations

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.mycompany.clientside {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires com.google.gson;
 
+    opens com.mycompany.clientside.serverhandler.message to com.google.gson;
     opens com.mycompany.clientside.controllers to javafx.fxml;
     exports com.mycompany.clientside;
 }
