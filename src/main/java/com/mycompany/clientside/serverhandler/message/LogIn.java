3fc046bf6914260ea37f0da74d5372da8027fabf
Eldeep1 <alieldeep111@gmail.com>
2026-01-02 06:34:03 +0200

    login endpoint?

---
+++ src/main/java/com/mycompany/clientside/serverhandler/message/LogIn.java
@@ -0,0 +1,32 @@
+package com.mycompany.clientside.serverhandler.message;
+
+public class LogIn {
+
+    private String userName;
+    private String pass;
+
+    public LogIn() {
+    }
+
+    public LogIn(String userName, String pass) {
+        this.userName = userName;
+        this.pass = pass;
+    }
+
+    // getters & setters
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
