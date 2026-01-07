d25f843a784d6b9bcc3f9719277278238cf7c4b3
Eldeep1 <alieldeep111@gmail.com>
2026-01-07 06:41:20 +0200

    adding login request

---
+++ src/main/java/com/mycompany/clientside/models/LoginRequest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clientside.models;
+
+/**
+ *
+ * @author Depogramming
+ */
+public class LoginRequest {
+    private String userName;
+    private String password;
+
+    public LoginRequest(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
