e5867340b155d2213e36df813feccd8f313fbfad
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-01-22 02:38:05 +0200

    Improved the ui ai difficulty screen

--- src/main/java/com/mycompany/clientside/controllers/DifficultyScreenController.java
+++ src/main/java/com/mycompany/clientside/controllers/DifficultyScreenController.java
@@ -8,14 +8,13 @@ import com.mycompany.clientside.App;
 import com.mycompany.clientside.Screens;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -24,54 +23,110 @@ import javafx.scene.control.ToggleGroup;
  */
 public class DifficultyScreenController implements Initializable {
 
-    @FXML
-    private ToggleGroup difficultyGroup;
 
-    /**
-     * Initializes the controller class.
-     */
-    public enum Difficulty {
+    @FXML
+    private Button easyButton;
+
+    @FXML
+    private Button mediumButton;
+
+    @FXML
+    private Button hardButton;
+    @FXML
+    private ImageView easyImage;
+
+    @FXML
+    private ImageView mediumImage;
+
+    @FXML
+    private ImageView hardImage;
+
+    private enum Difficulty {
         Easy,
         Mid,
         Hard
     }
 
-    Difficulty currentDifficulty;
+    private Difficulty currentDifficulty;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
         currentDifficulty = Difficulty.Easy;
-        difficultyGroup.selectedToggleProperty().addListener((obs, oldToggle, newToggle) -> {
-            if (newToggle == null) {
-                oldToggle.setSelected(true);
+        easyButton.getStyleClass().add("selected");
+        new Thread(() -> {
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
             }
-        });
+            Platform.runLater(() -> {
+                mediumImage.setVisible(false);
+                mediumImage.setManaged(false);
 
+                hardImage.setVisible(false);
+                hardImage.setManaged(false);
+            });
+        }).start();
     }
 
     @FXML
     private void clickEasy(ActionEvent event) {
-        currentDifficulty = Difficulty.Easy;
+        if (currentDifficulty != Difficulty.Easy){
+            easyButton.getStyleClass().add("selected");
+            mediumButton.getStyleClass().remove("selected");
+            hardButton.getStyleClass().remove("selected");
+            currentDifficulty = Difficulty.Easy;
 
+            easyImage.setVisible(true);
+            easyImage.setManaged(true);
+
+            mediumImage.setVisible(false);
+            mediumImage.setManaged(false);
+
+            hardImage.setVisible(false);
+            hardImage.setManaged(false);
+        }
     }
 
     @FXML
     private void clickMedium(ActionEvent event) {
-        currentDifficulty = Difficulty.Mid;
+        if (currentDifficulty != Difficulty.Mid){
+            mediumButton.getStyleClass().add("selected");
+            easyButton.getStyleClass().remove("selected");
+            hardButton.getStyleClass().remove("selected");
+            currentDifficulty = Difficulty.Mid;
 
+            easyImage.setVisible(false);
+            easyImage.setManaged(false);
+
+            mediumImage.setVisible(true);
+            mediumImage.setManaged(true);
+
+            hardImage.setVisible(false);
+            hardImage.setManaged(false);
+        }
     }
 
     @FXML
     private void clickHard(ActionEvent event) {
-        currentDifficulty = Difficulty.Hard;
+        if (currentDifficulty != Difficulty.Hard){
+            hardButton.getStyleClass().add("selected");
+            easyButton.getStyleClass().remove("selected");
+            mediumButton.getStyleClass().remove("selected");
+            currentDifficulty = Difficulty.Hard;
 
+            hardImage.setManaged(true);
+            hardImage.setVisible(true);
+
+            easyImage.setManaged(false);
+            easyImage.setVisible(false);
+
+            mediumImage.setManaged(false);
+            mediumImage.setVisible(false);
+        }
     }
 
     @FXML
     private void clickRecordGame(ActionEvent event) {
-        //here handel when user click record 
-
         try {
             AIGameScreenController.setDifficulty(currentDifficulty.ordinal());
             AIGameScreenController.setRecordMode(true);
